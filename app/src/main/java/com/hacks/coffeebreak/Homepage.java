f850ad344bdf03fc05f9708aab17ccf01189a759
idilaydin <idilaydin@gmail.com>
2021-02-06 18:07:10 -0500

    Added profile page

--- app/src/main/java/com/hacks/coffeebreak/Homepage.java
+++ app/src/main/java/com/hacks/coffeebreak/Homepage.java
@@ -3,16 +3,31 @@ package com.hacks.coffeebreak;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class Homepage extends AppCompatActivity {
 
+    private BottomAppBar mBottomAppBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+        mBottomAppBar = findViewById(R.id.bar);
+        setSupportActionBar(mBottomAppBar);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
 
 
 
