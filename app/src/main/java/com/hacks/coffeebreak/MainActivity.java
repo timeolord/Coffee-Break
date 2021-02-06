08e8a2151c602c925598a096dd5acaf75e370df6
Allan <timeolord6677@gmail.com>
2021-02-06 13:08:23 -0500

    Login Button and function

--- app/src/main/java/com/hacks/coffeebreak/MainActivity.java
+++ app/src/main/java/com/hacks/coffeebreak/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +16,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Login(View v){
-        startActivity(new Intent(MainActivity.this, MainScreen.class));
+        String email = ((EditText)findViewById(R.id.LoginEmail)).getText().toString();
+        String password = ((EditText)findViewById(R.id.LoginPass)).getText().toString();
+
+        if (email == "admin" && password == "admin"){
+            startActivity(new Intent(MainActivity.this, Homepage.class));
+        }
+        else {
+
+        }
     }
 
 }
