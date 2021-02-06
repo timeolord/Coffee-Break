1970d8de217ce0353d155db7a8a8ed84fb59a669
Allan <timeolord6677@gmail.com>
2021-02-06 13:00:39 -0500

    Login Button and function

--- app/src/main/java/com/hacks/coffeebreak/MainActivity.java
+++ app/src/main/java/com/hacks/coffeebreak/MainActivity.java
@@ -2,7 +2,9 @@ package com.hacks.coffeebreak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void Login(View v){
+        startActivity(new Intent(MainActivity.this, MainScreen.class));
+    }
+
 }
