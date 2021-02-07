3255998bf1413d4002fd1cf3ee7b66f00c25328d
idilaydin <idilaydin@gmail.com>
2021-02-06 22:19:30 -0500

    Popup Window

--- app/src/main/java/com/hacks/coffeebreak/Homepage.java
+++ app/src/main/java/com/hacks/coffeebreak/Homepage.java
@@ -2,13 +2,21 @@ package com.hacks.coffeebreak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.animation.ObjectAnimator;
+import com.daimajia.androidanimations.library.BaseViewAnimator;
 
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.google.android.material.bottomappbar.BottomAppBar;
-import com.hacks.coffeebreak.chat.ChatBox;
 
 public class Homepage extends AppCompatActivity {
 
@@ -24,18 +32,56 @@ public class Homepage extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu (Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
+
+
+
+    public class ZoomInAnimator extends BaseViewAnimator {
+        @Override
+        public void prepare(View target) {
+            getAnimatorAgent().playTogether(
+                    ObjectAnimator.ofFloat(target, "scaleX", 0.45f, 1),
+                    ObjectAnimator.ofFloat(target, "scaleY", 0.45f, 1),
+                    ObjectAnimator.ofFloat(target, "alpha", 0, 1)
+            );
+        }
     }
 
-    public void ToMessenger(View v){
-        startActivity(new Intent(Homepage.this, ChatBox.class));
+    public void onButtonShowPopupWindowClick(View view) {
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_window, null);
+
+
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+        //popupWindow.setAnimationStyle(R.style.Animation_Dialog);
+
+        YoYo.with(Techniques.BounceIn)
+                .duration(700)
+                .playOn(popupView);
+
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 
-    public void ToProfilePage(View v){
-        startActivity(new Intent(Homepage.this, ProfilePage.class));
-    }
+
+
+
 }
